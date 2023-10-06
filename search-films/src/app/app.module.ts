1ba86887c0ab9f6c9fb6ea7b72e56e956314942c
Tate <snehalata.tate@worldline.com>
2023-10-06 15:34:47 +0530

    update

--- search-films/src/app/app.module.ts
+++ search-films/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
+import { ChildComponent } from './component/child/child.component';
+import { LoginFormComponent } from './component/login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
